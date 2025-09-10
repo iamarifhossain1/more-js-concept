12f58318e10a274746ca7678831c1056f503788a
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-10 16:05:49 +0600

    asyncs

--- asyncs.js
+++ asyncs.js
@@ -2,7 +2,17 @@
 
 console.log("one");
 console.log("two");
-callThree();
+
+// setTimeout(callThree, 5000);
+
+setTimeout(() => {
+  console.log("this is new three inside set time out");
+}, 4000);
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
 console.log("four");
 console.log("five");
 
